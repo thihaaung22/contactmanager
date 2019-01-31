6121664690b9e39b23ad1b146104aa407f952213
thihaaung22 <thihaaung7779@gmail.com>
2019-01-30 21:48:01 -0800

    Updates

---
+++ precache-manifest.6f4fe8bced0cb5f48a07e161589a9034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75622d3688c5b69c7929",
+    "url": "/contactmanager/static/css/main.4ab803a3.chunk.css"
+  },
+  {
+    "revision": "75622d3688c5b69c7929",
+    "url": "/contactmanager/static/js/main.75622d36.chunk.js"
+  },
+  {
+    "revision": "af50d766e3860b801f91",
+    "url": "/contactmanager/static/css/1.0437e480.chunk.css"
+  },
+  {
+    "revision": "af50d766e3860b801f91",
+    "url": "/contactmanager/static/js/1.af50d766.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "9dfc814360758375337c75bce49a620e",
+    "url": "/contactmanager/index.html"
+  }
+];
